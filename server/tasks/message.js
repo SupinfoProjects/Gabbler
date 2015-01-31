0ed4d898a03a5d6343bed9ddabf6490fcbae0412
Samy Laumonier <159024@supinfo.com>
2015-01-31 18:56:11 +0100

    Extraction des tags lors de l'envoi d'un message

--- server/tasks/message.js
+++ server/tasks/message.js
@@ -5,6 +5,32 @@ Meteor.methods({
         }
 
         var user = Meteor.users.findOne({ _id: Meteor.userId() });
+        var matches = content.match(tagRegex);
+
+        if (matches) {
+            var tags = matches.map(function(tag) {
+                return tag.trim().substring(1);
+            });
+
+            var existingTags = Tags.find({ name: { $in: tags } }).fetch();
+
+            _.each(existingTags, function(tag) {
+                tags.splice(tags.indexOf(tag.name), 1);
+
+                Tags.update(this._id, { $set: {
+                    occurences: tag.occurences + 1
+                }});
+            });
+
+            _.each(tags, function(name) {
+                console.log('insert tag', name);
+
+                Tags.insert({
+                    name: name,
+                    occurences: 1
+                });
+            });
+        }
 
         Messages.insert({
             content: content,
